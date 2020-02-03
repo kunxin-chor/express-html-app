9e7171387af74b77f34ed1cd472fd3b5bc4fcb25
Paul <chorkunxin@yahoo.com>
2020-02-03 13:38:49 +0800

    Initial

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'));
+
+app.get('/products', (req, res) => {
+    res.json([
+        {
+            id:1,
+            name:'bicycle',
+            image: 'bicycle.png'
+        },
+        {
+            id:2,
+            name:'car',
+            image: 'car.png'
+        }
+    ])
+})
+
+app.get('/product_info', (req,res)=>{
+    let product_id = req.query.product_id;
+    res.json({
+        product_id
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
